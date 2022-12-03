9bd55a65149a35afc9ec7e1c6dbc1a5338a2d200
toast-uz <tatsuzo.osawa@gmail.com>
2022-12-03 20:14:04 +0900

    revise footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -21,7 +21,7 @@ const Layout = ({ children }: Props) => {
       </main>
 
       <footer className={styles.footer}>
-        <a href='https://twitter.com/ToastUz'> by ToastUz </a>
+        by <a href='https://twitter.com/ToastUz'>ToastUz</a>, source code is at <a href='https://github.com/toast-uz/ahc-stats'>GitHub</a>
       </footer>
       <Analytics />
     </div>
