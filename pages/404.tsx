262a96c588f49cb5660b4a8c03b2b758623fd0b4
toast-uz <tatsuzo.osawa@gmail.com>
2022-12-04 11:48:21 +0900

    Add home page link in Custom404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,20 @@
 import styles from '../styles/Home.module.css'
 import Script from "next/script";
+import Link from 'next/link';
 import Layout from "../components/Layout";
 
 export default function Custom404() {
   return (
     <Layout>
       <div className={styles.description}>
-        404 - Page Not Found....redirecting to Home Page after 3 seconds
+        404 - Page Not Found....
+        redirecting to <Link href="/">Home Page</Link> after 5 seconds
       </div>
       <Script id='redirect'>
         {`
         window.setTimeout(function() {
           document.location.href="/";
-        }, 3000);
+        }, 5000);
         `}
       </Script>
     </Layout>
