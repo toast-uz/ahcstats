730ed9cb10b486107eb1b177c902f70a26af1999
toast-uz <tatsuzo.osawa@gmail.com>
2023-02-06 07:46:13 +0900

    Change app name to ahcstats

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,21 +8,21 @@ const Layout = ({ children }: Props) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>ahc-stats</title>
-        <meta name="description" content="ahc-stats" />
+        <title>ahcstats</title>
+        <meta name="description" content="ahcstats" />
         <meta name="google-site-verification" content="_kLReeB5nmuUoJdyJkgjUbPwxR8cMkdqQ98997JO3Xw" />
       </Head>
 
       <main className={styles.main}>
         <header className={styles.title}>
-          <a href="https://atcoder.jp/" target="_blank" rel="noopener noreferrer">AtCoder</a> Heuristic Contest Statistics (ahc-stats)
+          <a href="https://atcoder.jp/" target="_blank" rel="noopener noreferrer">AtCoder</a> Heuristic Contest Statistics (ahcstats)
         </header>
         { children }
       </main>
 
       <footer className={styles.footer}>
         by <a href='https://twitter.com/ToastUz' target="_blank" rel="noopener noreferrer">ToastUz</a>
-        , source code is at <a href='https://github.com/toast-uz/ahc-stats' target="_blank" rel="noopener noreferrer">GitHub</a>
+        , source code is at <a href='https://github.com/toast-uz/ahcstats' target="_blank" rel="noopener noreferrer">GitHub</a>
       </footer>
     </div>
   )
